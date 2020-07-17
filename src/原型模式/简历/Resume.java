920bc54430b6c1e0536a2700f048ae309108f556
longjiangbo <longjiangbo@sinochem.com>
2020-07-17 09:19:20 +0800

    原型模式：简历

---
+++ src/原型模式/简历/Resume.java
@@ -0,0 +1,24 @@
+package 原型模式.简历;
+
+/**
+ * @program: bigtalkdesignpattensourceforjava
+ * @description: 简历类
+ * @author: LongJiangBo
+ * @create: 2020-07-16 21:46
+ **/
+public class Resume{
+    private String name;
+    private String sex;
+    private String age;
+    private String timeArea;
+    private String company;
+
+    public Resume(String name){
+        this.name = name;
+    }
+
+    //设置个人信息
+    public void SetPersonalInfo(String sex,String age){
+
+    }
+}
