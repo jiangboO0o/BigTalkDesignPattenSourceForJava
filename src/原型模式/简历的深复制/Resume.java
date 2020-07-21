cd2f8abee315731f9bb516cf2aa0c3ea5ca49ed9
longjiangbo <longjiangbo@sinochem.com>
2020-07-21 22:41:33 +0800

    原型模式：简历的深复制，添加注释

--- src/原型模式/简历的深复制/Resume.java
+++ src/原型模式/简历的深复制/Resume.java
@@ -17,6 +17,13 @@ public class Resume implements Cloneable {
         work = new WorkExperience();
     }
 
+    /**
+     * @Description: 提供Clone方法调用的私有构造函数，以便克隆“工作经历”的数据
+     * @Author: LongJiangBo
+     * @param work
+     * @return:
+     * @Date: 22:41 2020/7/21
+    */
     private Resume(WorkExperience work) throws CloneNotSupportedException {
         this.work = (WorkExperience)work.workExperienceClone();
     }
@@ -41,6 +48,13 @@ public class Resume implements Cloneable {
         System.out.println();
     }
 
+    /**
+     * @Description: 调用私有的构造方法，让“工作经历”克隆完成，然后再给这个“简历”独享的相关字段赋值，最终返回一个深复制的简历对象
+     * @Author: LongJiangBo
+     * @param
+     * @return: java.lang.Object
+     * @Date: 22:39 2020/7/21
+    */
     public Object resumeClone() throws CloneNotSupportedException {
         Resume obj = new Resume(this.work);
         obj.name = this.name;
